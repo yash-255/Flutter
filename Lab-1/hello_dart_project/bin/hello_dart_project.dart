3809ac660db5d95747cc48ad1b2fd23914b4a4b4
yash-255 <86880469+yash-255@users.noreply.github.com>
2022-09-28 05:10:41 -0700

    Add files via upload

---
+++ Lab-1/hello_dart_project/bin/hello_dart_project.dart
@@ -0,0 +1,6 @@
+import 'package:hello_dart_project/hello_dart_project.dart'
+    as hello_dart_project;
+
+void main() {
+  print('Hello world : ${hello_dart_project.calculate()}!');
+}
---
+++ Lab-1/lab1_tutorial2/bin/lab1_tutorial2.dart
@@ -0,0 +1,101 @@
+import 'dart:math';
+//import 'package:lab1_tutorial2/lab1_tutorial2.dart' as lab1_tutorial2;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${lab1_tutorial2.calculate()}!');
+
+  /*
+    //Commenting Code
+    
+    // This is a comment. It is not executed, Because it is a comment. : )
+    // /* comments for
+    // multiple
+    // lines */
+
+    // /* One comment Here
+    // /* And inside it is
+    // another comment. */
+    // Back to the first. */
+
+    /// This is a document comment.
+    /// For single line documentation
+    
+    // /**
+    //  * This is a 
+    //  * Block 
+    //  * Document
+    //  * Used
+    //  * for
+    //  * Big Blocks
+    //  * of Documents
+    //  */
+  */
+
+  /*
+    // Printing output
+    
+    // print("Hello, Dart Apprentice reader!");
+  */
+
+  /*
+    //Statements and expressions
+
+    // Statements
+
+    //This is simple statement
+    // print("Hello, Dart Apprentice reader!") ;
+
+    // single quote also works
+    // print('Hello, Single Quote');
+
+    //This is a complex Statement
+    // if(true){
+    //    print("Hello, Dart Apprentice reader!");
+    // }
+
+
+    // Expressions
+
+
+    // This Works
+    // 42;
+    // 3 + 2;
+    // "Hello There!!!";
+
+  */
+
+  /*
+    // Arithmetic operations
+
+    // Simple operations
+
+    // print(2+6);
+    // print(8*2);
+    // print(8/4);
+    // print(24-20);
+
+    // Decimal numbers
+    // print(22/7);
+    // print(3.1428);
+    // print(22 ~/ 3);
+    // print(22 % 8);
+
+    // Order of operations
+    // print((8 * 5) + ((5 - 3) / (1 + 2)));
+    // print(350 / 5 + 2);
+  */
+
+  //Math functions
+  // print(sin(45 * (pi / 180)));
+  // print(cos(135 * (pi / 180)));
+  // print(sqrt(2));
+  // print(max(5, 10));
+  // print(min(8,2));
+  // print(max(sqrt(2),pi/2));
+
+  // Now print the value of 1 overthe square root of 2 in Dart.
+  // Confirm that it equals the sine of 45°.
+  // They are same
+  print(1 / sqrt(2));
+  print(sin(45 * (pi / 180)));
+}
